4a89f6524e65cf7a0b5e79b10ccee36bcf95fc2c
Joseph Padfield <joseph.padfield@ng-london.org.uk>
2020-03-27 16:32:55 +0000

    Adding Timelines

--- build/build.php
+++ build/build.php
@@ -11,6 +11,8 @@ $raw_subpages = getRemoteJsonDetails("sub-pages.json", false, true);
 if (!is_array($raw_subpages) or count($raw_subpages) < 1)
 	{exit("\nERROR: Sorry your sub-pages.json file has not been opened correctly please check you json formatting and try vaildating it using a web-site similar to https://jsonlint.com/\n\n");}
 
+$specialPages = array("timeline");
+
 $menuList = array();
 $subpages = array();
 $bcs = array();
@@ -320,7 +322,7 @@ function writeTSPage ()
 	
 function writePage ($name, $d, $tnav=true)
 	{
-	global $subpages, $gdp, $menuList;
+	global $subpages, $gdp, $menuList, $specialPages;
 	
 	$pd = $gdp;
 		
@@ -333,8 +335,13 @@ function writePage ($name, $d, $tnav=true)
 	else
 		{$pd["topNavbar"] = buildTopNav ($d["bcs"][0]);
 		 $pd["breadcrumbs"] = buildBreadcrumbs ($d["bcs"]);}
-	
-	$home = parseFootNotes ($d["content"], $d["footnotes"], 1);
+
+	if (in_array($name, $specialPages))
+		{$ta = buildSpecialContent($name, $d["content"], $pd);
+		 $content = $ta[0];
+		 $pd = $ta[1];}
+	else
+		{$content = parseFootNotes ($d["content"], $d["footnotes"], 1);}
 				
 	$pd["grid"] = array(
 		"topjumbotron" => "<h2>$d[title]</h2>",
@@ -348,7 +355,7 @@ function writePage ($name, $d, $tnav=true)
 			array(
 				array (
 					"class" => "col-12 col-lg-12",
-					"content" => $home)
+					"content" => $content)
 				)));
 							
 	if ($d["content right"])
@@ -644,6 +651,108 @@ END;
 	ob_end_clean(); // Don't send output to client
 
 	return ($page_html);
-	}	
+	}
+
+// visualise with: https://mermaidjs.github.io/mermaid-live-editor test
+
+function buildSpecialContent ($name, $content, $pd)
+	{
+			
+	if ($name == "timeline")
+		{
+		$file = $content;
+		if (!file_exists($file))
+			{die("ERROR: $file missing\n");}
+		else
+			{
+			$dets = getRemoteJsonDetails($file, false, true);
+
+			if (!isset($dets["start date"]))
+				{die("ERROR: $file format problems - 'start date' not found\n");}
+		
+			$start = $dets["start date"];
+	
+			$prefs = array_keys($dets["groups"]);
+			$first = $prefs[0];
+
+			if (!isset($dets["project"])) {$dets["project"] = "Please add a project title";}
+			if (!isset($dets["margin"])) {$dets["margin"] = -3;}
+		
+			array_unshift($dets["groups"][$first]["stages"],
+				array("Add as a margin", "", $dets["margin"], $dets["margin"]));
+		
+			$str = "";
+			foreach ($dets["groups"] as $pref => $ga)
+				{
+				$str .= "\tsection $ga[title]\n";
+				$no = 0;
+				foreach ($ga["stages"] as $k => $a)
+					{
+					if ($a[1]) {$a[1] = "$a[1], ";}
+					$str .= "\t\t".$a[0]." :$a[1]$pref$no, ".dA($a[2]).
+						", ".dA($a[3])."\n";
+					$no++;
+					}
+				}
+
+			$pd["extra_css_scripts"][] =
+				"https://mermaidjs.github.io/mermaid-live-editor/src.96cd87af.css";
+			$pd["extra_js_scripts"][] =
+				"https://cdn.jsdelivr.net/npm/mermaid@8.4.0/dist/mermaid.min.js";
+			$pd["extra_onload"] .= "
+	mermaid.ganttConfig = {
+    titleTopMargin:25,
+    barHeight:20,
+    barGap:4,
+    topPadding:50,
+    sidePadding:50
+		}
+console.log(mermaid.render);
+  mermaid.initialize({startOnLoad:true, flowchart: { 
+    curve: 'basis' 
+  }});";
+			//use to hide the label used for the first line which is just in place to provide a margin/padding on the left.
+			$pd["extra_css"] .= "
+g a {color:inherit;}
+#".$first."0-text {display:none;}";
+
+		ob_start();
+		echo <<<END
+	<div class="mermaid">
+gantt
+       dateFormat  YYYY-MM-DD
+       title $dets[project]	
+       $str
+	</div>
+END;
+			$html = ob_get_contents();
+			ob_end_clean(); // Don't send output to client
+			}	
+		}
+	return (array($html, $pd));
+	}
+	
+function dA ($v)
+	{
+	global $start;
+	$a = explode(",", $v);
+	$m = intval($a[0]);
+	if(isset($a[1]))
+		{$d = intval($a[1]);}
+	else
+		{$d = 0;}
+	$date=new DateTime($start); // date object created.
+
+	$invert = 0;
+	if ($m < 0 or $d < 0)
+		{$invert = 1;
+		 $m = abs($m);
+		 $d = abs($d);}
+	$di = new DateInterval('P'.$m.'M'.$d.'D');
+	$di->invert = $invert;
+	$date->add($di); // inerval of 1 year 3 months added
+	$new = $date->format('Y-m-d'); // Output is 2020-Aug-30
+	return($new);
+	}
 
 ?>
